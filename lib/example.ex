bd9e3a357a0127225f519e52f2caf3015bfcc78d
nBryan <nbryan133@hotmail.com>
2025-06-30 14:36:43 -0400

    basics of generating project files and how to complile them smoothly

--- lib/example.ex
+++ lib/example.ex
@@ -1,18 +1,12 @@
 defmodule Example do
-  @moduledoc """
-  Documentation for `Example`.
-  """
+  use Application
+  alias UUID
+  def start(_type, _args) do
+    Example.main()
+    Supervisor.start_link([], strategy: :one_for_one)
+  end
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Example.hello()
-      :world
-
-  """
-  def hello do
+  def main do
     IO.puts(:world)
   end
 end
