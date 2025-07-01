9c9241d9c47c9a6e84f5dc3fba999634fbdd81e8
nBryan <nbryan133@hotmail.com>
2025-07-01 10:02:55 -0400

    messed around with atoms, strings, and the syntax for elixir switch statements

--- lib/example.ex
+++ lib/example.ex
@@ -1,12 +1,23 @@
+#class
 defmodule Example do
   use Application
-  alias UUID
+
   def start(_type, _args) do
     Example.main()
     Supervisor.start_link([], strategy: :one_for_one)
   end
 
+  #function
   def main do
-    IO.puts(:world)
+    #declaring variables
+    name = "Nate"
+    status = Enum.random([:Gold, :Silver, :Bronze, :"no status"])
+
+    #switch statement
+    case status do
+      :Gold -> IO.puts("Welcome to the fancy lounge, #{name}!")
+      :"no status" -> IO.puts("Members only beyond this point")
+      _ -> IO.puts("#{status} status members not permitted please upgrade status to continue.")
+    end
   end
 end
