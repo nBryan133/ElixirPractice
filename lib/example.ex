8ec859699a0a13808af356d964fc90ca2c7f404e
nBryan <nbryan133@hotmail.com>
2025-07-01 10:57:49 -0400

    did some practice with maps and using modules + structs

--- lib/example.ex
+++ lib/example.ex
@@ -1,4 +1,12 @@
-#class
+defmodule Membership do
+  defstruct [:type, :price]
+end
+
+defmodule User do
+  defstruct [:name, :membership]
+end
+
+#Module(class equivalent)
 defmodule Example do
   use Application
 
@@ -29,12 +37,70 @@ defmodule Example do
     #IO.puts(date_time.year)
     #IO.inspect(date_time)
 
-    #example func to tell how much time till new year
+    #example process to tell how much time till new year
     #timeTillNY()
 
+    #example process to learn about tuples
+    #tupleFun()
+
+    #example process to learn about maps
+    mapFun()
 
   end
 
+  #process(function) to learn how maps work
+  def mapFun do
+    #map of membership tiers and their prices made through a module
+    gold_membership = %Membership{type: :gold, price: 25}
+    silver_membership = %Membership{type: :silver, price: 20}
+    bronze_membership = %Membership{type: :bronze, price: 15}
+    none_membership = %Membership{type: :null, price: 0}
+
+    #tuple of users and their membership tiers
+    users = [
+      %User{name: "Nate", membership: gold_membership},
+      %User{name: "Morgan", membership: gold_membership},
+      %User{name: "Mike", membership: silver_membership},
+      %User{name: "John", membership: bronze_membership},
+      %User{name: "Jerry", membership: none_membership}
+    ]
+
+    Enum.each(users, fn %User{name: name, membership: membership} ->
+      IO.puts("#{name} has a #{membership.type} membership paying #{membership.price}.")
+    end)
+
+  end
+
+  #process(function) to learn how tuples work
+  def tupleFun do
+    memberships = {:bronze, :silver}
+    memberships = Tuple.insert_at(memberships, tuple_size(memberships), :gold)
+    IO.inspect(memberships)
+
+    prices = {5, 10, 15}
+    avg = Tuple.sum(prices) / tuple_size(prices)
+    IO.puts(avg)
+
+    IO.puts(
+      "Average price form #{elem(memberships, 0)} #{elem(memberships, 1)} #{elem(memberships, 2)} is #{avg}"
+    )
+
+    #tuple of users and their membership tiers
+    users = [
+      {"Nate", :gold},
+      {"Morgan", :gold},
+      {"Mike", :silver},
+      {"Andy", :bronze}
+    ]
+
+    #for loop equivalent
+    Enum.each(users, fn{name, membership} ->
+      IO.puts("#{name} has a #{membership} membership")
+    end)
+
+  end
+
+  #process (function) to get time till new year
   def timeTillNY do
     time = DateTime.new!(Date.new!(2026, 1, 1), Time.new!(0, 0, 0, 0), "Etc/UTC")
     time_till = DateTime.diff(time, DateTime.utc_now())
